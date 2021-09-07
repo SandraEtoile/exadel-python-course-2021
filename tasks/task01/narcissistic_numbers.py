a8f7847c71cab6496fc5b843a68895ebbb845a6c
akalodkina <akalodkina@exadel.com>
2021-09-07 22:49:00 +0300

    task 01 - fixed comments

--- tasks/task01/narcissistic_numbers.py
+++ tasks/task01/narcissistic_numbers.py
@@ -1,19 +1,26 @@
-def countDigits(n):
-    count = 0
+def count_digits(number, digits):
+    n = number
+    number_of_digits = 0
     while n != 0:
+        digits.append(n % 10)
         n //= 10
-        count += 1
-    return count
+        number_of_digits += 1
+    return number_of_digits
 
 
-narcissistic_numbers = []
-for i in range(1, 1001):
-    number_of_digits = countDigits(i)
-    digits = [int(x) for x in str(i)]
+def is_narcissistic_number(number):
+    digits = []
+    number_of_digits = count_digits(number, digits)
     powers = []
     for d in digits:
         power = pow(d, number_of_digits)
         powers.append(power)
-    if sum(powers) == i:
+    return sum(powers) == number
+
+
+narcissistic_numbers = []
+for i in range(1, 155):
+    if is_narcissistic_number(i):
         narcissistic_numbers.append(i)
+
 print(narcissistic_numbers)
