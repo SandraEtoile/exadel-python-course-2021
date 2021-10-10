32279139b971f09cfe214da3cdcac09a2ec9fe25
akalodkina <akalodkina@exadel.com>
2021-10-10 18:25:07 +0300

    task08 - added tests

--- tasks/task08/test_shapes.py
+++ tasks/task08/test_shapes.py
@@ -1,100 +1,108 @@
-from typing import NamedTuple
-import abc
-import math
+import unittest
+
+from tasks.task08.shapes import Circle, Point2D, Rectangle, Square, Shape2DCollection
 
 
-class Point2D(NamedTuple):
-    x: float
-    y: float
+class TestShapes(unittest.TestCase):
 
-    def __str__(self):
-        return f"({self.x}, {self.y})"
+    def test_circle_area(self):
+        circle = Circle([1, 2], 5)
+        area = circle.area
+        self.assertEqual(area, 78.5, "Correct value is 78.5")
+
+    def test_circle_contains_point(self):
+        circle = Circle([1, 2], 5.0)
+        point = Point2D(1.0, 3.0)
+        self.assertTrue(circle.__contains__(point), "Correct value is True")
+
+    def test_circle_contains_negative_point(self):
+        circle = Circle([-1, 2], 5.0)
+        point = Point2D(1.0, 1.0)
+        self.assertTrue(circle.__contains__(point), "Correct value is True")
+
+    def test_circle_contains_point_on_line(self):
+        circle = Circle([1, 2], 5.0)
+        point = Point2D(1.0, 7.0)
+        self.assertTrue(circle.__contains__(point), "Correct value is True")
+
+    def test_circle_not_contains_point(self):
+        circle = Circle([1, 2], 5.0)
+        point = Point2D(10.0, 7.0)
+        self.assertFalse(circle.__contains__(point), "Correct value is False")
+
+    def test_rectangle_area(self):
+        rectangle = Rectangle([1, 2], 5, 10)
+        area = rectangle.area
+        self.assertEqual(area, 50, "Correct value is 50")
+
+    def test_rectangle_contains_point(self):
+        rectangle = Rectangle([1, 2], 5, 10)
+        point = Point2D(2.0, 3.0)
+        self.assertTrue(rectangle.__contains__(point), "Correct value is True")
+
+    def test_rectangle_contains_negative_point(self):
+        rectangle = Rectangle([-1, 2], 5, 10)
+        point = Point2D(2.0, 3.0)
+        self.assertTrue(rectangle.__contains__(point), "Correct value is True")
+
+    def test_rectangle_contains_point_on_line(self):
+        rectangle = Rectangle([1, 2], 5, 10)
+        point = Point2D(1.0, 3.0)
+        self.assertTrue(rectangle.__contains__(point), "Correct value is True")
+
+    def test_rectangle_not_contains_point(self):
+        rectangle = Rectangle([1, 2], 5, 10)
+        point = Point2D(12.0, 8.0)
+        self.assertFalse(rectangle.__contains__(point), "Correct value is False")
+
+    def test_square_area(self):
+        square = Square([1, 2], 5)
+        area = square.area
+        self.assertEqual(area, 25, "Correct value is 25")
+
+    def test_square_contains_point(self):
+        square = Square([1, 2], 5)
+        point = Point2D(2.0, 3.0)
+        self.assertTrue(square.__contains__(point), "Correct value is True")
+
+    def test_square_contains_negative_point(self):
+        square = Square([-1, 2], 5)
+        point = Point2D(2.0, 3.0)
+        self.assertTrue(square.__contains__(point), "Correct value is True")
+
+    def test_square_contains_point_on_line(self):
+        square = Square([1, 2], 5)
+        point = Point2D(1.0, 3.0)
+        self.assertTrue(square.__contains__(point), "Correct value is True")
+
+    def test_square_not_contains_point(self):
+        square = Square([1, 2], 5)
+        point = Point2D(12.0, 8.0)
+        self.assertFalse(square.__contains__(point), "Correct value is False")
+
+    def test_shape2d_collection_area(self):
+        square = Square([1, 2], 5)
+        rectangle = Rectangle([1, 2], 5, 10)
+        circle = Circle([1, 2], 5)
+        shape_2d_collection = Shape2DCollection([square, rectangle, circle])
+        self.assertEqual(shape_2d_collection.area, 153.5, "Correct value is 153.5")
+
+    def test_shape2d_collection_contains_point(self):
+        square = Square([10, 2], 5)
+        rectangle = Rectangle([10, 2], 5, 10)
+        circle = Circle([1, 2], 5)
+        point = Point2D(1.0, 3.0)
+        shape_2d_collection = Shape2DCollection([square, rectangle, circle])
+        self.assertTrue(shape_2d_collection.__contains__(point))
+
+    def test_shape2d_collection_not_contains_point(self):
+        square = Square([10, 2], 5)
+        rectangle = Rectangle([10, 2], 5, 10)
+        circle = Circle([10, 2], 5)
+        point = Point2D(1.0, 3.0)
+        shape_2d_collection = Shape2DCollection([square, rectangle, circle])
+        self.assertFalse(shape_2d_collection.__contains__(point))
 
 
-class Shape2D(abc.ABC):
-    @property
-    @abc.abstractmethod
-    def area(self) -> float:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def __contains__(self, point: Point2D) -> bool:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def __str__(self) -> str:
-        raise NotImplementedError
-
-
-class Rectangle(Shape2D):
-    def __init__(self, bottom_left: dict, width, length):
-        self.bottom_left = bottom_left
-        self.width = width
-        self.length = length
-
-    @property
-    def area(self) -> float:
-        return self.width * self.length
-
-    def calculate_top_right_coordinates(self):
-        return self.bottom_left[0] + self.length, self.bottom_left[1] + self.width
-
-    def __contains__(self, point: Point2D) -> bool:
-        top_right = self.calculate_top_right_coordinates()
-        if self.bottom_left[0] < point.x < top_right[0] and self.bottom_left[1] < point.y < top_right[1]:
-            return True
-        else:
-            return False
-
-    def __str__(self) -> str:
-        return f"({self.bottom_left}, {self.width}, {self.length})"
-
-
-class Square(Rectangle):
-    def __init__(self, side_size):
-        super().__init__(side_size, side_size)
-
-    # @Rectangle.area.setter
-    # def area(self) -> float:
-    #     return self.side_size * self.side_size
-
-    # def calculate_top_right_coordinates(self):
-    #     return self.bottom_left[0] + self.side_size, self.bottom_left[1] + self.side_size
-
-
-class Circle(Shape2D):
-    def __init__(self, center: dict, radius):
-        self.center = center
-        self.radius = radius
-
-    @property
-    def area(self) -> float:
-        return pow(self.radius, 2) * math.pi
-
-    def __contains__(self, point: Point2D) -> bool:
-        if (pow(point.x - self.center[0], 2) + pow(point.y - self.center[1], 2)) <= pow(self.radius, 2):
-            return True
-        else:
-            return False
-
-    def __str__(self) -> str:
-        return f"({self.center}, {self.radius})"
-
-
-class Shape2DCollection(Shape2D):
-    def __init__(self, shapes: list):
-        self.shapes = shapes
-
-    @property
-    def area(self) -> float:
-        return sum(shape.area for shape in self.shapes)
-
-    def __contains__(self, point: Point2D) -> bool:
-        for shape in self.shapes:
-            if shape.__contains__(point):
-                return True
-            else:
-                return False
-
-    def __str__(self) -> str:
-        return f"({self.shapes})"
+if __name__ == '__main__':
+    unittest.main()
